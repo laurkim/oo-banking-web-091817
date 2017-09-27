0bf70fc230fc4fe76aaeffc04907dd40671ae469
laurkim <laurakeem@gmail.com>
2017-09-27 17:35:33 -0400

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
 class BankAccount
 
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def name
+    @name
+  end
+
+  def deposit(money)
+    self.balance += money
+    self.balance
+  end
+
+  def withdraw(money)
+    self.balance -= money
+    self.balance
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?(*args)
+    (self.status == "open" && self.balance > 0) ? true : false
+  end
+
+  def close_account
+    @status = "closed"
+  end
+
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,50 @@
 class Transfer
   # your code here
+
+  attr_accessor :sender, :receiver, :amount, :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?(*args)
+    # binding.pry
+    account = BankAccount.new(args)
+    # binding.pry
+    account.valid?
+    self.sender.valid?
+    self.receiver.valid?
+    #LOOK INTO THIS: see if this is the best possible way to test
+  end
+
+  def execute_transaction
+    # binding.pry
+    sender_acct = self.sender
+    receiver_acct = self.receiver
+    # binding.pry
+    if self.valid?(sender_acct, receiver_acct) && (sender_acct.balance > self.amount) && (self.status == "pending")
+      # binding.pry
+      receiver_acct.deposit(self.amount)
+      sender_acct.withdraw(self.amount)
+      self.status = "complete"
+      # binding.pry
+      # binding.pry
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      # binding.pry
+      self.receiver.withdraw(self.amount)
+      self.sender.deposit(self.amount)
+      self.status = "reversed"
+    end
+  end
+
 end
